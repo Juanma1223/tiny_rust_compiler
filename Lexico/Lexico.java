8ed40e4a68aac593db64492a6d377cbac7f3d43a
Juanma Fernandez <juanmanuelf12@gmail.com>
2023-03-12 20:23:49 -0300

    Se agrego el diagrama de clases al archivo readme

---
+++ Lexico/Lexico.java
@@ -0,0 +1,7 @@
+package Lexico;
+
+public class Lexico{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Diagrama de clases
+
+## https://app.diagrams.net/#G1grRs0qVO1AAXRcOkJwSi4J-KmnTfq8PC
