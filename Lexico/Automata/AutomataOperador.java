8f745741fd6c6f8e0a33c71bad4c1da1e8d657c2
marielvol <marielvolman@hotmail.com>
2023-03-14 11:36:38 -0300

    se añadieron los distintos operadores

--- Lexico/Automata/AutomataOperador.java
+++ Lexico/Automata/AutomataOperador.java
@@ -16,19 +16,103 @@ public class AutomataOperador extends Automata{
     @Override
     public Token reconocerToken(BufferedReader lector){
         Token token = new Token();
-        // Establecemos el tipo de token Operador
-        token.establecerToken("Operador");
         String lexema = "";
         // Leemos hasta encontrar EOF o el fin del token
         try {
             int c;
             while((c = lector.read()) != -1) {
               char character = (char) c;
-              // Si encontramos un caracter que corresponda a una letra, se trata de un identificador valido
-              // y continuamos construyendo el lexema
+
               if(c == 40){
                 lexema = lexema+character;
                 token.establecerToken("op_par_abre");
+              }
+              if(c == 41){
+                lexema = lexema+character;
+                token.establecerToken("op_par_cierra");
+              }
+              if(c == 91){
+                lexema = lexema+character;
+                token.establecerToken("op_cor_abre");
+              }
+              if(c == 93){
+                lexema = lexema+character;
+                token.establecerToken("op_cor_cierra");
+              }
+              if(c == 123){
+                lexema = lexema+character;
+                token.establecerToken("op_llave_abre");
+              }
+              if(c == 125){
+                lexema = lexema+character;
+                token.establecerToken("op_llave_cierra");
+              }
+              if(c == 59){
+                lexema = lexema+character;
+                token.establecerToken("op_punto_coma");
+              }
+              if(c == 44){
+                lexema = lexema+character;
+                token.establecerToken("op_coma");
+              }
+              if(c == 58){
+                lexema = lexema+character;
+                token.establecerToken("op_dos_puntos");
+              }
+              if(c == 46){
+                lexema = lexema+character;
+                token.establecerToken("op_punto");
+              }
+              //Agregar igual
+              if(c == 61){
+                lexema = lexema+character;
+                token.establecerToken("op_asignacion");
+              }
+              //Agregar menor o igual
+              if(c == 60){
+                lexema = lexema+character;
+                token.establecerToken("op_menor");
+              }
+              //Agregar mayor o igual
+              if(c == 62){
+                lexema = lexema+character;
+                token.establecerToken("op_mayor");
+              }
+              if(c == 43){
+                lexema = lexema+character;
+                token.establecerToken("op_suma");
+              }
+              //Agregar op-flecha
+              if(c == 45){
+                lexema = lexema+character;
+                token.establecerToken("op_resta");
+              }
+              if(c == 42){
+                lexema = lexema+character;
+                token.establecerToken("op_mult");
+              }
+              if(c == 47){
+                lexema = lexema+character;
+                token.establecerToken("op_div");
+              }
+              if(c == 37){
+                lexema = lexema+character;
+                token.establecerToken("op_mod");
+              }
+              //Agregar otro &
+              if(c == 38){
+                lexema = lexema+character;
+                token.establecerToken("op_and");
+              }
+              //Agregar otro |
+              if(c == 124){
+                lexema = lexema+character;
+                token.establecerToken("op_or");
+              }
+              //Agregar distinto
+              if(c == 33){
+                lexema = lexema+character;
+                token.establecerToken("op_not");
               }
               // Encontramos un espacio, devolvemos el token
               if(c == 32){
