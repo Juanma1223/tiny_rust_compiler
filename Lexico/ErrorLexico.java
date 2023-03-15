c0383d911061fe45c86a41b0a2d40b1c647f04ff
marielvol <marielvolman@hotmail.com>
2023-03-15 16:34:51 -0300

    se agrego la clase ErrorLexico

---
+++ Lexico/ErrorLexico.java
@@ -0,0 +1,9 @@
+package Lexico;
+
+public class ErrorLexico extends RuntimeException{
+    public ErrorLexico(int fila, int columna, String mensaje){
+        System.out.println("ERROR: LEXICO");
+        System.out.println("| NUMERO DE LINEA: | NUMERO DE COLUMNA: | DESCRIPCION: |");
+        System.out.println("|LINEA " + Integer.toString(fila) + " | COLUMNA " + Integer.toString(columna) + " | " + mensaje + "|");
+    }
+}
