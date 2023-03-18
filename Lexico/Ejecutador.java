219031cf81610b6ee3647563754929d43214929a
Juanma Fernandez <juanmanuelf12@gmail.com>
2023-03-18 01:32:11 -0300

    Implementacion de pila de tokens en el ejecutador

--- Lexico/Ejecutador.java
+++ Lexico/Ejecutador.java
@@ -2,25 +2,33 @@ package Lexico;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Ejecutador {
     public static void main(String[] args) throws IOException {
         // Abrimos el archivo y almacenamos su informacion
-        File file = new File("test/test.rs");
+        File file = new File("test/test3.rs");
         Lexico anaLexico = new Lexico(file);
-       while(true) {
+        // Pila en la que guardamos los tokens para posteriormente imprimirlos por
+        // pantalla
+        ArrayList<Token> tokens = new ArrayList<Token>();
+        // Leemos todos los tokens
+        while (true) {
             Token token = anaLexico.sigToken(false);
             // Imprimimos el token con su lexema y el numero de linea y columna donde se
             // encuentra
-            // Imprimimos el token con su lexema y el numero de linea y columna donde se
-            // encuentra
-            if (token.obtenerToken() == null){
+            if (token.obtenerToken() == null) {
                 break;
             }
-            if(token.obtenerToken() != "Comentario"){
-                System.out.println("| " + token.obtenerToken() + " | " + token.obtenerLexema() + " |" + " LINEA "
-                        + token.obtenerFila() + " (COLUMNA " + token.obtenerColumna() + ") |");
+            if (token.obtenerToken() != "Comentario") {
+               // Insertamos los tokens que no sean comentarios
+               tokens.add(token);
             }
+        }
+        // Imprimimos los tokens
+        for(Token token:tokens){
+            System.out.println("| " + token.obtenerToken() + " | " + token.obtenerLexema() + " |" + " LINEA "
+            + token.obtenerFila() + " (COLUMNA " + token.obtenerColumna() + ") |");
         }
     }
 }
