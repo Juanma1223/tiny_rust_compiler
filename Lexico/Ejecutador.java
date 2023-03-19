865e52da504ea51d0728b74df08b8584da5ac66b
Juanma Fernandez <juanmanuelf12@gmail.com>
2023-03-19 11:08:50 -0300

    se agrego el archivo .jar

--- Lexico/Ejecutador.java
+++ Lexico/Ejecutador.java
@@ -1,6 +1,7 @@
 package Lexico;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -8,36 +9,54 @@ public class Ejecutador {
     public static void main(String[] args) throws IOException {
 
         // Leemos el argumento que es pasado por stdin
-        if(args.length == 0){
+        if (args.length < 1) {
             // No recibimos nombre de archivo y por tanto no podemos proceder
-            new ErrorLexico(0, 0, "No se ingreso ningun archivo!");
+            new ErrorLexico(0, 0, "No se ingreso archivo de codigo fuente!");
         }
 
         // Abrimos el archivo y almacenamos su informacion
-        File file = new File(args[0]);
-        Lexico anaLexico = new Lexico(file);
+        File archivo = new File(args[0]);
+        Lexico anaLexico = new Lexico(archivo);
         // Pila en la que guardamos los tokens para posteriormente imprimirlos por
         // pantalla
         ArrayList<Token> tokens = new ArrayList<Token>();
+        boolean leyendo = true;
         // Leemos todos los tokens
-        while (true) {
+        while (leyendo) {
             Token token = anaLexico.sigToken(false);
             // Imprimimos el token con su lexema y el numero de linea y columna donde se
             // encuentra
             if (token.obtenerToken() == null) {
-                break;
+                leyendo = false;
             }
-            if (token.obtenerToken() != "Comentario") {
-               // Insertamos los tokens que no sean comentarios
-               tokens.add(token);
+            if (token.obtenerToken() != "Comentario" && leyendo == true) {
+                // Insertamos los tokens que no sean comentarios
+                tokens.add(token);
             }
         }
-        System.out.println("CORRECTO: ANALISIS LEXICO");
-        System.out.println("| TOKEN | LEXEMA | NUMERO DE LINEA (NUMERO DE COLUMNA) |");
-        // Imprimimos los tokens
-        for(Token token:tokens){
-            System.out.println("| " + token.obtenerToken() + " | " + token.obtenerLexema() + " |" + " LINEA "
-            + token.obtenerFila() + " (COLUMNA " + token.obtenerColumna() + ") |");
+        if (args.length == 2) {
+            // Creamos el archivo de salida
+            try {
+                FileWriter escritor = new FileWriter(args[1]);
+                escritor.write("CORRECTO: ANALISIS LEXICO\n");
+                escritor.write("| TOKEN | LEXEMA | NUMERO DE LINEA (NUMERO DE COLUMNA) |\n");
+                for (Token token : tokens) {
+                    escritor.write("| " + token.obtenerToken() + " | " + token.obtenerLexema() + " |" + " LINEA "
+                            + token.obtenerFila() + " (COLUMNA " + token.obtenerColumna() + ") |");
+                    escritor.write("\n");
+                }
+            } catch (IOException e) {
+                System.out.println("Ocurrio un error mientras se escribir al archivo");
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("CORRECTO: ANALISIS LEXICO");
+            System.out.println("| TOKEN | LEXEMA | NUMERO DE LINEA (NUMERO DE COLUMNA) |");
+            // Imprimimos los tokens
+            for (Token token : tokens) {
+                System.out.println("| " + token.obtenerToken() + " | " + token.obtenerLexema() + " |" + " LINEA "
+                        + token.obtenerFila() + " (COLUMNA " + token.obtenerColumna() + ") |");
+            }
         }
     }
 }
