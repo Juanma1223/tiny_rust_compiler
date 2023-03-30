9cd3f2cfdd4731eff6d162fdb41a3ea6742571dc
marielvol <marielvolman@hotmail.com>
2023-03-30 12:12:37 -0300

    Se añadieron los test

--- Lexico/test/test9.rs
+++ Lexico/test/test9.rs
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-/
-/*CORRECTO*/
-class Prueba {
-	I32: a;
-	pub Str: b;
-	Array I32: c;
-	Char: d;
-	Bool: e;
-	create(){// Esto es un comentario simple
-		a = 42;
-	}
-	fn get_a() -> I32 {
-        // Esto es un comentario simple
-		return a;
-	}
-	static fn imprimo_algo() -> void {
-		IO.out_string("hola mundo");
-	}
-}
-fn main () {
-	c = new I32[1+2];
-}
-///
-=======
 /*CORRECTO*/
 ///comentario
 class Prueba {
@@ -45,4 +20,3 @@ class Prueba {
 fn main () {
 	c = new I32[1+2];
 }
->>>>>>> d4d3087e82fc5c63d60b91212434dbaf3eeb78b6
