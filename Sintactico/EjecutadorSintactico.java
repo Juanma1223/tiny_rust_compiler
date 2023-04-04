65abe43d7d3506af59edf72995b998688362c41a
marielvol <marielvolman@hotmail.com>
2023-04-04 15:41:54 -0300

    modif dir archivo ejecutador sintactico

--- Sintactico/EjecutadorSintactico.java
+++ Sintactico/EjecutadorSintactico.java
@@ -2,7 +2,7 @@ package Sintactico;
 
 public class EjecutadorSintactico {
     public static void main(String[] args) {
-        Sintactico anaSintactico = new Sintactico("/mnt/hdd/Facultad/Facultad/4to_año/Compiladores/Compilador/tiny_rust_compiler/Sintactico/test/test.rs");
-        System.out.println("Exito!");
+        Sintactico anaSintactico = new Sintactico("test/test.rs");
+        System.out.println("CORRECTO: ANALISIS SINTACTICO");
     }
 }
