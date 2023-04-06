76025445212506e34bb9350f0d94d61984236fd6
marielvol <marielvolman@hotmail.com>
2023-04-06 10:30:53 -0300

    se agregaron test

---
+++ Sintactico/test/test10.rs
@@ -0,0 +1,7 @@
+/*ERROR: LINEA 2 | COLUMNA 14 | Se esperaba: : o {, se encontró: pepe*/
+class Prueba pepe {
+
+}
+fn main () {
+    
+}
---
+++ Sintactico/test/test11.rs
@@ -0,0 +1,7 @@
+/*ERROR: LINEA 3 | COLUMNA 2 | Se esperaba: }, se encontró: +*/
+class Prueba {
+	+
+}
+fn main () {
+		
+}
