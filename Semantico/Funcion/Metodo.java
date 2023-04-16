b5be6aca3108e2769b2598e8e6062e273d070be4
Juanma Fernandez <juanmanuelf12@gmail.com>
2023-04-16 19:21:03 -0300

    getters de la clase metodo

--- Semantico/Funcion/Metodo.java
+++ Semantico/Funcion/Metodo.java
@@ -5,7 +5,28 @@ import Semantico.Tipo;
 public class Metodo extends Funcion{
     private String nombre;
     private boolean esEstatico;
-    private boolean esPublic;
+    private boolean esPublico;
     private Tipo tipoRetorno;
     private int posicion;
+
+
+    public String obtenerNombre(){
+        return this.nombre;
+    }
+
+    public boolean obtenerEsEstatico(){
+        return this.esEstatico;
+    }
+
+    public boolean obtenerEsPublico(){
+        return this.esPublico;
+    }
+
+    public Tipo obtenerTipoRetorno(){
+        return this.tipoRetorno;
+    }
+
+    public int obtenerPosicion(){
+        return this.posicion;
+    }
 }
