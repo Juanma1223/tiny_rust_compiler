68cf97505c8b11e2514e34de95939d1a1c0f155f
marielvol <marielvolman@hotmail.com>
2023-05-19 19:31:05 -0300

    "modif nodo variable"

--- Semantico/Nodo/NodoVariable.java
+++ Semantico/Nodo/NodoVariable.java
@@ -3,7 +3,6 @@ package Semantico.Nodo;
 import Lexico.Token;
 import Semantico.Clase;
 import Semantico.Funcion.Funcion;
-import Semantico.Funcion.Metodo;
 import Semantico.Tipo.Tipo;
 
 public class NodoVariable extends NodoExpresion {
@@ -11,16 +10,11 @@ public class NodoVariable extends NodoExpresion {
     Token token;
     Tipo tipo;
 
-    public NodoVariable(Funcion metodoContenedor, Clase claseContenedora, Nodo padre, Token token) {
+    public NodoVariable(Funcion metodoContenedor, Clase claseContenedora, Token token) {
         super(metodoContenedor, claseContenedora);
         this.token = token;
         this.metodoContenedor = metodoContenedor;
         this.claseContenedora = claseContenedora;
-    }
-
-    public NodoVariable(Funcion metodoContenedor, Clase claseContenedora, Token token) {
-        super(metodoContenedor, claseContenedora);
-        this.token = token;
     }
 
 }
