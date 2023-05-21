0add8d4f7ea241f53d76b23efceaf4fc77117c09
Juanma Fernandez <juanmanuelf12@gmail.com>
2023-05-21 15:05:59 -0300

    implementacion de checkeo de tipos y cantidad en llamadas a metodos

--- Semantico/Nodo/NodoLlamadaMetodo.java
+++ Semantico/Nodo/NodoLlamadaMetodo.java
@@ -10,6 +10,7 @@ import Semantico.Funcion.Metodo;
 import Semantico.Funcion.Constructor;
 import Semantico.Tipo.Tipo;
 import Semantico.Tipo.TipoReferencia;
+import Semantico.Variable.Parametro;
 
 public class NodoLlamadaMetodo extends NodoExpresion {
 
@@ -51,45 +52,57 @@ public class NodoLlamadaMetodo extends NodoExpresion {
             if (token.obtenerToken().equals("id_clase")) {
                 Clase infoClase = tablaDeSimbolos.obtenerClasePorNombre(token.obtenerLexema());
                 if (infoClase == null) {
-                    new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(), "No se puede crear un objeto de la clase "
-                    + token.obtenerLexema() + " porque no esta definida.",true);
+                    new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(),
+                            "No se puede crear un objeto de la clase "
+                                    + token.obtenerLexema() + " porque no esta definida.",
+                            true);
                 }
+                Constructor constructor = infoClase.obtenerConstructor();
+                this.checkeoCantArgumentos(constructor);
                 this.tipo = new TipoReferencia(clase);
             } else if (estatico == true) {
                 // Si el método es estático ya sabemos la clase padre
                 Clase infoClase = tablaDeSimbolos.obtenerClasePorNombre(clase);
                 if (infoClase == null) {
                     new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(), "La clase del metodo estatico "
-                    + token.obtenerLexema() + " no esta definida.",true);
+                            + token.obtenerLexema() + " no esta definida.", true);
                 }
                 Metodo infoMetodo = infoClase.obtenerMetodoPorNombre(token.obtenerLexema());
                 // Si el metodo es nulo, el metodo no se encuentra en la tabla de simbolos
                 if (infoMetodo == null) {
                     new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(), "El metodo " + token.obtenerLexema()
-                            + " no esta definido para la clase " + clase,true);
+                            + " no esta definido para la clase " + clase, true);
                 }
+                this.checkeoCantArgumentos(infoMetodo);
                 this.tipo = infoMetodo.obtenerTipoRetorno();
             } else {
                 // Para verificar el tipo de retorno buscamos la informacion del metodo en la
                 // tabla de simbolos
-                if (padre instanceof NodoVariable){
-                    // Si el padre es un NodoVariable, el metodo debe estar definido en la clase de dicha variable
+                if (padre instanceof NodoVariable) {
+                    // Si el padre es un NodoVariable, el metodo debe estar definido en la clase de
+                    // dicha variable
                     Clase infoClase = tablaDeSimbolos.obtenerClasePorNombre(padre.obtenerTipoClase().obtenerTipo());
                     Metodo infoMetodo = infoClase.obtenerMetodoPorNombre(token.obtenerLexema());
                     // Si el metodo es nulo, el metodo no se encuentra en la tabla de simbolos
                     if (infoMetodo == null) {
-                        new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(), "El metodo " + token.obtenerLexema()
-                            + " no esta definido para la clase " + padre.obtenerTipoClase().obtenerTipo(),true);
+                        new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(),
+                                "El metodo " + token.obtenerLexema()
+                                        + " no esta definido para la clase " + padre.obtenerTipoClase().obtenerTipo(),
+                                true);
                     }
+                    this.checkeoCantArgumentos(infoMetodo);
                     this.tipo = infoMetodo.obtenerTipoRetorno();
                 } else {
                     // Sino, el metodo debe estar definido en la clase contenedora
                     Metodo infoMetodo = claseContenedora.obtenerMetodoPorNombre(token.obtenerLexema());
                     // Si el metodo es nulo, el metodo no se encuentra en la tabla de simbolos
                     if (infoMetodo == null) {
-                        new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(), "El metodo " + token.obtenerLexema()
-                            + " no esta definido para la clase " + claseContenedora.obtenerNombre(),true);
+                        new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(),
+                                "El metodo " + token.obtenerLexema()
+                                        + " no esta definido para la clase " + claseContenedora.obtenerNombre(),
+                                true);
                     }
+                    this.checkeoCantArgumentos(infoMetodo);
                     this.tipo = infoMetodo.obtenerTipoRetorno();
                 }
             }
@@ -100,6 +113,27 @@ public class NodoLlamadaMetodo extends NodoExpresion {
     @Override
     public void checkeoTipos() {
         this.obtenerTipo();
+    }
+
+    // Este metodo checkea que la cantidad y tipo de argumentos enviados en la
+    // llamada coincida con los del metodo original
+    public void checkeoCantArgumentos(Funcion infoMetodo) {
+        ArrayList<Parametro> argOrdenados = infoMetodo.obtenerParamsOrdenados();
+        // Checkeamos que la cantidad de parametros sea la misma
+        if (argOrdenados.size() != argumentos.size()) {
+            new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(),
+                    "Cantidad de argumentos incorrecta en el llamado a funcion!", true);
+        }
+        // Checkeamos que el tipo de los parametros coincida
+        for (int i = 0; i < argOrdenados.size(); i++) {
+            Tipo tipoArgLlamado = argumentos.get(i).obtenerTipo();
+            Tipo tipoArgDeclarado = argOrdenados.get(i).obtenerTipo();
+            if (!tipoArgLlamado.obtenerTipo().equals(tipoArgDeclarado.obtenerTipo())) {
+                new ErrorSemantico(token.obtenerFila(), token.obtenerColumna(),
+                        "El argumento en la posicion " + i + " deberia ser de tipo " + tipoArgDeclarado.obtenerTipo(),
+                        true);
+            }
+        }
     }
 
     public String toJson() {
--- Semantico/testSemantico2/test_prueba.rs
+++ Semantico/testSemantico2/test_prueba.rs
@@ -1,15 +1,15 @@
-/*ERROR: Método no declarado*/
+/*ERROR: Parámetros incompatibles en tipo*/
 class Prueba {
-	I32: a;
-	Str: b;
-	create(){
-		// a=0;
-		// b="hola";
+	fn suma(I32:a,I32:b) -> I32 {
+		I32 : c;
+		c = a+b;
+		return c;
 	}
-
 }
 fn main () {
 	Prueba : p1;
+	I32 : s0;
+	Str : s1;
 	p1 = new Prueba();
-	(p1.suma());
+	(p1.suma(s0,s1));
 }
